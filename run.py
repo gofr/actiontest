ac216f788116e3ba1c0ad0db0a840049a2f97f63
gofr <32750931+gofr@users.noreply.github.com>
2020-12-29 17:19:18 +0100

    Test Python Pillow script and action

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
-print('test')
+from PIL import Image
+
+
+if __name__ == '__main__':
+    img = Image.new('RGB', (32, 32))
+    name = 'icon.png'
+    img.save(name)
+    print(f'Image "{name}" created successfully.')
